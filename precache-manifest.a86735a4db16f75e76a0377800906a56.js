adec8c75340a4565f9a05e59d8334ab0bc835dc1
ruptaker <rups1773@gmail.com>
2023-02-20 13:33:05 -0600

    Updates

---
+++ precache-manifest.a86735a4db16f75e76a0377800906a56.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c21aa1bba10f28e49a24",
+    "url": "/ruptaker/react_portfolio.git/static/css/main.1c1b4c32.chunk.css"
+  },
+  {
+    "revision": "c21aa1bba10f28e49a24",
+    "url": "/ruptaker/react_portfolio.git/static/js/main.c21aa1bb.chunk.js"
+  },
+  {
+    "revision": "20c8d902f5a34bd23fad",
+    "url": "/ruptaker/react_portfolio.git/static/js/1.20c8d902.chunk.js"
+  },
+  {
+    "revision": "53ff9757a7119c385894",
+    "url": "/ruptaker/react_portfolio.git/static/js/runtime~main.53ff9757.js"
+  },
+  {
+    "revision": "10523783f1e245f4442b724223c2d357",
+    "url": "/ruptaker/react_portfolio.git/static/media/CV.10523783.pdf"
+  },
+  {
+    "revision": "46beb24f925529935d36307ec914edbb",
+    "url": "/ruptaker/react_portfolio.git/static/media/ME.46beb24f.png"
+  },
+  {
+    "revision": "7c014e0c49946951201c22cbbda8f6a1",
+    "url": "/ruptaker/react_portfolio.git/static/media/me-about.7c014e0c.jpg"
+  },
+  {
+    "revision": "c6c6455e574b613a0d4f3ebc4e98b40a",
+    "url": "/ruptaker/react_portfolio.git/static/media/portfolio1.c6c6455e.jpg"
+  },
+  {
+    "revision": "3c263319be6a6b41fe78777f43bb2ed9",
+    "url": "/ruptaker/react_portfolio.git/static/media/portfolio2.3c263319.jpg"
+  },
+  {
+    "revision": "d17330b0db7434a370ce3626287a3c36",
+    "url": "/ruptaker/react_portfolio.git/static/media/portfolio3.d17330b0.jpg"
+  },
+  {
+    "revision": "9ed200aa4c5215e0c7d6d44eb453e960",
+    "url": "/ruptaker/react_portfolio.git/static/media/portfolio4.9ed200aa.jpg"
+  },
+  {
+    "revision": "232a17ccbed8fb02d505a2d223f6b009",
+    "url": "/ruptaker/react_portfolio.git/static/media/portfolio5.232a17cc.jpg"
+  },
+  {
+    "revision": "a41eb4c327c40b542d61fb8535e5d273",
+    "url": "/ruptaker/react_portfolio.git/static/media/portfolio6.a41eb4c3.jpg"
+  },
+  {
+    "revision": "f9d8d13f8ce2a7b81e3693269db4937a",
+    "url": "/ruptaker/react_portfolio.git/index.html"
+  }
+];
